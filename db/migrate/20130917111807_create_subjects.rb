87f0290ccbf790a85a12655b8544780290495ee9
Lee Kearney <leekearney1@gmail.com>
2013-09-19 12:12:43 +0100

    Revert "Rename visible to visable"
    This reverts commit 5a29a5e9fdba476e431e2433e3b61ba6dd59f801.

--- db/migrate/20130917111807_create_subjects.rb
+++ db/migrate/20130917111807_create_subjects.rb
@@ -3,7 +3,7 @@ class CreateSubjects < ActiveRecord::Migration
     create_table :subjects do |t|
     t.string "name"
     t.integer "position"
-    t.boolean "visable", :default => false
+    t.boolean "visible", :default => false
     
       t.timestamps
     end

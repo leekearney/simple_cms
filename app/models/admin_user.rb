8c433b0de4f7514cd8ba5980cbb6246951402ca3
Lee Kearney <leekearney1@gmail.com>
2013-10-11 17:13:29 +0100

    User Authentication/salting/hashing passwords

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,3 +1,4 @@
+require 'digest/sha1'
 class AdminUser < ActiveRecord::Base
   # attr_accessible :title, :body
 
@@ -6,7 +7,9 @@ class AdminUser < ActiveRecord::Base
   has_many :section_edits
   has_many :sections, :through => :section_edits
 
-  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  attr_accessor :password
+
+  #EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 
  validates_presence_of :first_name
  validates_length_of :first_name, :maximum => 25
@@ -20,6 +23,37 @@ class AdminUser < ActiveRecord::Base
  validates_format_of :email, :with => EMAIL_REGEX
  validates_confirmation_of :email
 
+ #only on create, so other attributes of this iser can be changed
+ validates_length_of :password, :within => 8..25, :on => :create
+
+ before_save :create_hashed_password
+ after_save :clear_password
 
   scope :named, lambda {|first, last| where(:first_name => first, :last_name => last)}
+end
+
+attr_protected :hashed_password, :salt
+
+#def self.make_salt(username="")
+	Digest::SHA1.hexdigest("Use #{username} with #{Time.now} to make salt")
+end
+
+def self.hash_with_salt(password="", salt="")
+	Digest::SHA1.hexdigest("put #{salt} on the #{password}")
+end
+
+private
+
+def craete_hashed_password
+	#when ever :password has a value hashing is needed
+	unless password.blank?
+
+	self.salt = AdminUser.make_salt(username) if salt.blank?
+	self.hashed_password = AdminUser.hash_with_salt(password, salt)
+	end
+end
+
+def clear_password
+
+	self.password = nil
 end
